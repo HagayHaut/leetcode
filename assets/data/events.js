e3b46f675dee2c6c4ad57f0892683cc9c6aceaba
Hagay Haut <hagayhaut@gmail.com>
2022-04-23 00:19:26 -0400

    add uneventful events for background fun

---
+++ assets/data/events.js
@@ -0,0 +1,21 @@
+const keys = document.querySelectorAll('.btn');
+
+keys[0].onclick = () => console.log('Ctrl');
+keys[1].onclick = () => console.log('Shift');
+keys[2].onclick = () => console.log('J');
+keys[3].onclick = () => console.log('command');
+keys[4].onclick = () => console.log('option');
+keys[5].onclick = () => console.log('J');
+
+
+const title = document.querySelector('#title')
+
+title.onclick = (e) => toggleButton(e);
+
+function toggleButton(e) {
+    if(e.target.textContent == 'Local IDE REPLit') {
+        e.target.textContent = 'Didn\'t Do Much';
+    } else {
+        e.target.textContent = 'Local IDE REPLit';
+    }
+}
