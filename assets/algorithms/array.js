c0f57e5667e08cfb59ef31949f7de3e73f9b10b0
Hagay Haut <hagayhaut@gmail.com>
2022-04-22 22:17:36 -0400

    add a couple array methods

--- assets/algorithms/array.js
+++ assets/algorithms/array.js
@@ -1,2 +1,14 @@
 // ARRAY ALGORITHMS WORTH SAVING
 
+// Rotate array left one position
+function rotateLeft(arr) {
+    return arr.slice(1).concat(arr[0]);
+}
+
+// rotate string right one position
+function rotateRight(arr) {
+    let last = arr[arr.length-1];
+    let missingLast = arr.slice(0,-1);
+    missingLast.unshift(last);
+    return missingLast;
+}
