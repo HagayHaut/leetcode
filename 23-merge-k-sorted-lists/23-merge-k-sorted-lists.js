32df72a832412ae4f74d54d0b1056c2bab87bf3c
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-08-25 10:43:24 -0400

    Time: 452 ms (26.51%), Space: 47.1 MB (87.27%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+
+
+const mergeKLists = function(lists) {
+    if (!lists.length) return null;
+    if (lists.length < 2) return lists[0];
+    return lists.reduce(merge);
+}
+
+const merge = (a, b) => {
+    let dummy = new ListNode(0),
+        tail = dummy;
+    while (a && b) {
+        if (a.val < b.val) {
+            tail.next = a;
+            a = a.next;
+        } else {
+            tail.next = b;
+            b = b.next;
+        }
+        tail = tail.next;
+    }
+
+    tail.next = a || b;
+    return dummy.next;
+}
