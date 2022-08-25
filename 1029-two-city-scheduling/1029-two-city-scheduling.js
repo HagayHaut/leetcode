e5b286b52ea57a1b8d76515c4e19c866acdc1f6f
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-08-25 11:30:32 -0400

    Time: 72 ms (91.59%), Space: 43.2 MB (33.64%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} costs
+ * @return {number}
+ */
+
+// can do with memoization or dp
+// but better with greedy algorithm
+// get difference of costB - costA and sort by diff (store c1 & c2)
+// first half go to cityA, second half to cityA
+const twoCitySchedCost = function(costs) {
+    const diffs = costs.map(([a, b]) => [b - a, a, b]);
+    diffs.sort((a, b) => a[0] - b [0]);
+    let res = 0;
+    diffs.forEach(([dif, a, b], i) => {
+        res += (i < Math.floor(diffs.length/2) ? b : a);
+    })
+    return res;
+};
