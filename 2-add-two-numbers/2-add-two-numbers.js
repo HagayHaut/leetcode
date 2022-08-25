68c5509f5c20f26d8baeff12eb675db96ab5b14f
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-08-25 00:38:17 -0400

    Time: 98 ms (98.22%), Space: 46.4 MB (98.86%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.js
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const addTwoNumbers = function(l1, l2) {
+    const dummy = new ListNode();
+    
+    let curr = dummy,
+        carry = 0;
+    
+    while (l1 || l2 || carry) {
+        const v1 = l1 ? l1.val : 0,
+              v2 = l2 ? l2.val : 0,
+              val = v1 + v2 + carry;
+        
+        carry = Math.floor(val/10);
+        curr.next = new ListNode(val % 10);
+        curr = curr.next;
+        
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next;
+    }
+    
+    return dummy.next;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
