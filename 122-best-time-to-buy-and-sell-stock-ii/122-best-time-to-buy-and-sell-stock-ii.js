c8509a3133810d97739700d09723b8c4a60161bb
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-08-25 12:55:38 -0400

    Time: 55 ms (99.50%), Space: 41.6 MB (99.00%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit = function(prices) {
+    let result = 0;
+    for (let i = 0; i < prices.length-1; i++) {
+        if (prices[i] < prices[i+1]) {
+            result += prices[i+1] - prices[i];
+        }
+    }
+    return result;
+};
