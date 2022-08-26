ee63246d44721e4c04966cd3084bd868b3565838
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-08-26 16:15:34 -0400

    Time: 165 ms (33.31%), Space: 50 MB (75.86%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.js
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.js
@@ -11,13 +11,12 @@
  * @param {TreeNode} root2
  * @return {TreeNode}
  */
+
+
 const mergeTrees = function(r1, r2) {
     if (!r1 && !r2) return null;
-    
     const root = new TreeNode((r1 ? r1.val : 0) + (r2 ? r2.val : 0));
-    
     root.left = mergeTrees(r1 ? r1.left : null, r2 ? r2.left : null);
     root.right = mergeTrees(r1 ? r1.right : null, r2 ? r2.right : null);
-    
     return root;
 };
