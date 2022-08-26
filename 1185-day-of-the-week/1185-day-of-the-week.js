8e59753560bb0921484e3df00b29c563c45f6e7f
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-08-25 20:43:37 -0400

    Time: 78 ms (70.67%), Space: 42.6 MB (23.56%) - LeetHub

---
+++ 1185-day-of-the-week/1185-day-of-the-week.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} day
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+var dayOfTheWeek = function(day, month, year) {
+   const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+   return days[new Date(year, month-1, day).getDay()]
+};
