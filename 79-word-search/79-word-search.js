4873ccc80b7f2e7b97ae5a2e8db347578541ad10
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-08-26 10:02:07 -0400

    Time: 2829 ms (13.53%), Space: 48.4 MB (42.79%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -8,34 +8,30 @@
 
 // O(n*m*(4^n))
 const exist = function(board, word) {
-    const ROWS = board.length,
-          COLS = board[0].length,
+    const n = board.length,
+          m = board[0].length,
           path = new Set();
     
     const dfs = (r, c, i) => {
         if (i === word.length) return true;
-        
         if (
-            r < 0 || c < 0 || 
-            r >= ROWS || c >= COLS || 
-            word[i] !== board[r][c] ||
-            path.has(`${r},${c}`)
+            r >= 0 &&
+            c >= 0 &&
+            r < n &&
+            c < m &&
+            !path.has(`${r},${c}`) &&
+            word[i] === board[r][c]
         ) {
-            return false;
+            path.add(`${r},${c}`);
+            const res = dfs(r+1,c,i+1) || dfs(r-1,c,i+1) || dfs(r,c+1,i+1) || dfs(r,c-1,i+1);
+            path.delete(`${r},${c}`);
+            return res;
         }
-        
-        path.add(`${r},${c}`);
-        
-        const res = dfs(r + 1, c, i + 1) ||
-                    dfs(r - 1, c, i + 1) ||
-                    dfs(r, c + 1, i + 1) ||
-                    dfs(r, c - 1, i + 1) 
-        path.delete(`${r},${c}`)
-        return res;
-    } 
+        return false;
+    }
     
-    for (let r = 0; r < ROWS; r++) {
-        for (let c = 0; c < COLS; c++) {
+    for (let r = 0; r < n; r++) {
+        for (let c = 0; c < m; c++) {
             if (board[r][c] === word[0]) {
                 if (dfs(r, c, 0)) return true;
             }
