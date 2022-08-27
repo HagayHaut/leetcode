13ac48a68b784f13e552080cd06d94ae01d97727
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-08-26 23:52:11 -0400

    Update 1029-two-city-scheduling.js

--- 1029-two-city-scheduling/1029-two-city-scheduling.js
+++ 1029-two-city-scheduling/1029-two-city-scheduling.js
@@ -9,7 +9,7 @@
 // first half go to cityB, second half to cityA
 const twoCitySchedCost = function(costs) {
     const diffs = costs.map(([a, b]) => [b - a, a, b]);
-    diffs.sort((a, b) => a[0] - b [0]);
+    diffs.sort((a, b) => a[0] - b[0]);
     let res = 0;
     diffs.forEach(([dif, a, b], i) => {
         res += (i < Math.floor(diffs.length/2) ? b : a);
