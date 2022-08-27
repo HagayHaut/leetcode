bb6e5e5b916d1c5e4a4d8fe7db9cba56a2de88ea
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-08-27 16:31:46 -0400

    Time: 128 ms, Memory: 43.7 MB - LeetHub

---
+++ binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+
+// just BFS with a queue, but
+// have to have inner loop to 
+// empty items of level in queue
+const levelOrder = function(root) {
+    const result = [],
+          q = [root];
+    
+    if (!root) return result;
+    
+    while (q.length) {
+        const qLen = q.length,
+              level = [];
+        
+        for (let i = 0; i < qLen; i++) {
+            const node = q.shift();
+            level.push(node.val);
+            if (node.left) q.push(node.left);
+            if (node.right) q.push(node.right);
+        }
+        
+        result.push(level);
+    }
+    
+    return result;
+};
