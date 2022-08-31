84044d40535bedcf611439bcab11243d555ea51e
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-08-31 13:27:47 -0400

    Time: 258 ms (30.12%), Space: 55 MB (13.78%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.js
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.js
@@ -16,52 +16,51 @@
 
 // O(n*m) time & space
 const pacificAtlantic = function(heights) {
-    const rows = heights.length,
-          cols = heights[0].length;
+    const ROWS = heights.length,
+          COLS = heights[0].length,
+          dirs = [[1,0], [-1,0],[0,1],[0,-1]];
     
-    
-    const pac = Array(rows).fill().map(() => Array(cols).fill(false)),
-          atl = Array(rows).fill().map(() => Array(cols).fill(false));
+    const pac = Array(ROWS).fill().map(_ => Array(COLS).fill(false)),
+          atl = Array(ROWS).fill().map(_ => Array(COLS).fill(false));
     
     const dfs = (r, c, ocean, prevHeight) => {
         if (
-            r < 0 || 
-            c < 0 || 
-            r >= rows || 
-            c >= cols || 
-            ocean[r][c] || 
+            r < 0 ||
+            c < 0 ||
+            r >= ROWS ||
+            c >= COLS ||
+            ocean[r][c] ||
             heights[r][c] < prevHeight
         ) {
             return;
         }
         
         ocean[r][c] = true;
-        dfs(r+1, c, ocean, heights[r][c]);
-        dfs(r-1, c, ocean, heights[r][c]);
-        dfs(r, c+1, ocean, heights[r][c]);
-        dfs(r, c-1, ocean, heights[r][c]);
+        dirs.forEach(([row, col]) => dfs(r+row,c+col,ocean,heights[r][c]));
     }
     
-    // go through top and bottom
-    for (let c = 0; c < cols; c++) {
-        dfs(0, c, pac, -Infinity);
-        dfs(rows-1, c, atl, -Infinity);
-    }
-    // go through left and right
-    for (let r = 0; r < rows; r++) {
+    for (let r = 0; r < ROWS; r++) {
         dfs(r, 0, pac, -Infinity);
-        dfs(r, cols-1, atl, -Infinity);
+        dfs(r, COLS - 1, atl, -Infinity);
     }
     
-    const intersection = [];
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < cols; c++) {
+    for (let c = 0; c < COLS; c++) {
+        dfs(0, c, pac, -Infinity);
+        dfs(ROWS - 1, c, atl, -Infinity);
+    }
+    
+    const res = [];
+    
+    for (let r = 0; r < ROWS; r++) {
+        for (let c = 0; c < COLS; c++) {
             if (pac[r][c] && atl[r][c]) {
-                intersection.push([r,c]);
+                res.push([r,c]);
             }
         }
     }
-    return intersection;
+    
+    
+    return res;
 };
 
 
