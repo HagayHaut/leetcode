02d17a62e58d6c92f4e35fa9fb0954c80bb2588a
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-08-30 22:33:57 -0400

    Time: 304 ms (33.84%), Space: 50.6 MB (83.37%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,18 +3,16 @@
  * @return {boolean}
  */
 const isPalindrome = function(x) {
-        if (x < 0) return false;
+    if (x < 0) return false;
     
     let div = 1;
-    
-    while (x >= div*10) div *= 10;
+    while (x >= div * 10) div *= 10;
     
     while (x) {
-        const left = Math.floor(x / div),
-              right = x % 10;
-        
+        const left = Math.floor(x / div);
+        const right = x % 10;
         if (left !== right) return false;
-        x = Math.floor((x % div) / 10);
+        x = ~~((x % div) / 10);
         div /= 100;
     }
     
