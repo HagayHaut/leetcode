2825eb47d189a02b4ce21d49b7788f5c5fd21384
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-03 11:08:24 -0400

    Time: 101 ms (60.72%), Space: 43.5 MB (65.98%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.js
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 const titleToNumber = function(c) {
-    let res = 0;
-    const ACode = 'A'.charCodeAt(0)
+    let result = 0;
+    const ACode = 'A'.charCodeAt(0);
     for (let i = 0; i < c.length; i++) {
-        const charVal = c[c.length - 1 - i].charCodeAt(0) - ACode + 1;
-        res += charVal * (26**i);
+        const charVal = c[c.length - 1 - i].charCodeAt(0) - ACode + 1; 
+        result += charVal * (26**i);
     }
-    return res;
+    return result;
 };
