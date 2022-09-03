a8b3a58f6782f7418ec805d65227e5bf0d9f61ad
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-02 23:57:18 -0400

    Time: 80 ms (92.36%), Space: 45.2 MB (35.25%) - LeetHub

--- 46-permutations/46-permutations.js
+++ 46-permutations/46-permutations.js
@@ -5,25 +5,23 @@
 
 // backtracking dfs
 const permute = function(nums) {
-    const res = [];
-
-    const dfs = function (res, arr, nums) {
-        const len = nums.length;
-        let tmp1, tmp2;
-
-        if (!len) return res.push(arr);
-  
-        for (let i = 0; i < len; i++) {
-            tmp1 = [...arr];
-            tmp1.push(nums[i]);
-
-            tmp2 = [...nums];
-            tmp2.splice(i, 1);
-
-            dfs(res, tmp1, tmp2);
-      }
-    };
+    const result = [];
+    const n = nums.length;
     
-    dfs(res, [], nums);
-    return res;
+    const dfs = (set = new Set()) => {
+        if (set.size === n) {
+            return result.push([...set]);
+        }
+        
+        for (let i = 0; i < n; i++) {
+            if (!set.has(nums[i])) {
+                set.add(nums[i]);
+                dfs(set);
+                set.delete(nums[i]);
+            }
+        }
+    }
+    
+    dfs();
+    return result;
 };
