d402eae72a860275aad4cd758d00bb375e54d6b4
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-03 22:29:10 -0400

    Time: 81 ms (63.72%), Space: 42.7 MB (9.70%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findPeakElement = function(nums) {
+    const n = nums.length;
+    const safeGet = (i) => (
+        i === -1 || i === n ? -Infinity : nums[i]
+    );
+    
+    const bs = (l, r) => {
+        if (l > r) return -1;
+        const m = ~~((l + r) / 2);
+        const curr = safeGet(m);
+        const next = safeGet(m + 1);
+        const prev = safeGet(m - 1);
+        if (prev < curr && next < curr) return m;
+        
+        if (curr > next) return bs(l, m - 1);
+        else return bs(m + 1, r);
+    }
+    
+    return bs(0, n - 1);
+};
