076939e974c712c03a430bd9482c59a3af8645a1
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-05 16:27:48 -0400

    Time: 126 ms (51.18%), Space: 46.4 MB (60.15%) - LeetHub

--- 55-jump-game/55-jump-game.js
+++ 55-jump-game/55-jump-game.js
@@ -7,6 +7,9 @@
 // and figure out smallest index that 
 // can reach the last spot
 // if that index is 0, return true
+
+
+// O(n) time, O(1) space
 const canJump = function(nums) {
     let lastTrue = nums.length - 1;
     for (let i = nums.length - 2; i >= 0; i--) {
