d04e0f73d2051fb83509adf088484d69dc3c5439
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-04 21:54:41 -0400

    Time: 121 ms (62.10%), Space: 47.6 MB (41.13%) - LeetHub

---
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {TreeNode}
+ */
+const removeLeafNodes = function(root, target) {
+    if (!root) return null;
+    if (root.left) root.left = removeLeafNodes(root.left, target);
+    if (root.right) root.right = removeLeafNodes(root.right, target);
+    if (!root.left && !root.right && root.val === target) {
+        return null;
+    }
+        
+    return root
+};
