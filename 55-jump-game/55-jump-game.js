d16d86dcf5cd97bffd91a4e3124e67949fdebf60
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-05 14:46:49 -0400

    Time: 76 ms (95.93%), Space: 46.1 MB (80.24%) - LeetHub

--- 55-jump-game/55-jump-game.js
+++ 55-jump-game/55-jump-game.js
@@ -3,15 +3,12 @@
  * @return {boolean}
  */
 const canJump = function(nums) {
-    const dp = Array(nums.length).fill(false);
-    dp[dp.length - 1] = true;
-    let lastTrue = dp.length - 1;
+    let lastTrue = nums.length - 1;
     for (let i = nums.length - 2; i >= 0; i--) {
         const dist = lastTrue - i;
         if (nums[i] >= dist) {
-            dp[i] = true;
             lastTrue = i;
         }
     }
-    return dp[0];
+    return !lastTrue;
 };
