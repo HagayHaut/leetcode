008b14b16e621f3ba1dc62f1f2c7457da12905d5
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-05 18:03:41 -0400

    Time: 93 ms (34.63%), Space: 41.8 MB (53.81%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
@@ -6,8 +6,7 @@ const tribonacci = function(n) {
     const tribs = [0,1,1];
     if (n < 3) return tribs[n];
     for (let i = 3; i <= n; i++) {
-        const next = tribs.slice(-3).reduce((a, b) => a + b);
-        tribs.push(next);
+        tribs.push(tribs.slice(-3).reduce((a, b) => a + b));
     }
     return tribs[tribs.length - 1];
 };
