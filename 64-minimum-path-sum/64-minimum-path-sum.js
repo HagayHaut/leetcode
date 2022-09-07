f83cffa43b7c3fb018fcbcb133d7dd13df5518b9
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-07 19:57:26 -0400

    Time: 131 ms (22.42%), Space: 44.7 MB (47.99%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const minPathSum = function(grid) {
+    const safeGet = (r, c) => (
+        r < 0 || c < 0 ? Infinity : grid[r][c]
+    );
+    
+    grid.forEach((row, i) => {
+        row.forEach((n, j) => {
+            if (!i && !j) return;
+            grid[i][j] += Math.min(safeGet(i - 1, j), safeGet(i, j - 1));
+        })
+    })
+    
+    return grid[grid.length - 1][grid[0].length - 1];
+};
