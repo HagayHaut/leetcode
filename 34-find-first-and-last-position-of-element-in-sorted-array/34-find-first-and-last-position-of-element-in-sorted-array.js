c20c35c1f5ff1758689b4a080094c18b331096d6
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-10 15:29:12 -0400

    Time: 105 ms (25.60%), Space: 42.1 MB (94.51%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -8,23 +8,23 @@
 // go right @ target to find right edge
 // go left second time to find left edge
 const searchRange = function(nums, target) {
-    const bs = (leftBias) => {
+    const bs = (goLeft) => {
         let l = 0,
             r = nums.length - 1,
-            i = -1;
+            res = -1;
         
         while (l <= r) {
-            const m = (l + r) >> 1;
-            if (target > nums[m]) l = m + 1;
-            else if (target < nums[m]) r = m - 1;
-            else {
-                i = m;
-                if (leftBias) r = m - 1;
+            const m = ~~((l + r) / 2);
+            if (nums[m] === target) {
+                res = m;
+                if (goLeft) r = m - 1;
                 else l = m + 1;
             }
+            else if (nums[m] < target) l = m + 1;
+            else r = m - 1;
         }
         
-        return i;
+        return res;
     }
     
     return [bs(true), bs(false)];
