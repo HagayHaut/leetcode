248ea7ffb0f1b4fd77b600539519b2cfa91d7ffc
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-12 10:53:06 -0400

    Time: 135 ms (10.42%), Space: 44.6 MB (17.14%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -17,13 +17,12 @@ const mergeTwoLists = function(list1, list2) {
         if (list1.val < list2.val) {
             tail.next = list1;
             list1 = list1.next;
-            tail = tail.next;
         } else {
             tail.next = list2;
             list2 = list2.next;
-            tail = tail.next;
         }
+        tail = tail.next;
     }
-    tail.next = list1 || list2;
+    tail.next = list1 ?? list2;
     return dummy.next;
 };
