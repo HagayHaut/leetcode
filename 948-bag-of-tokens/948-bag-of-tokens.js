75665ff559d619ec43e2c7ee1a97f9fbbf56ee2c
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-12 14:28:52 -0400

    Time: 107 ms (63.64%), Space: 44 MB (69.70%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.js
+++ 948-bag-of-tokens/948-bag-of-tokens.js
@@ -3,6 +3,11 @@
  * @param {number} power
  * @return {number}
  */
+
+// greedy approach
+// maximize power when losing score, 
+// minimize power cost when gaining score
+
 const bagOfTokensScore = function(tokens, power) {
     let [score, maxScore] = [0, 0];
     let [l , r] = [0, tokens.length - 1];
@@ -10,17 +15,20 @@ const bagOfTokensScore = function(tokens, power) {
     tokens.sort((a, b) => a - b);
     
     while (l <= r) {
+        // minimize power when buying score
         if (power >= tokens[l]) {
             power -= tokens[l];
             score++;
             maxScore = Math.max(score, maxScore);
             l++;
         }
+        // maximize power when spending score
         else if (score > 0) {
             score--;
             power += tokens[r];
             r--;
         }
+        // can't 
         else {
             break;
         }
