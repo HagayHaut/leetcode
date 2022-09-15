7a5aa73ba9b4a6f2da40806856c60ba4aa0493bd
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-14 23:49:19 -0400

    Time: 748 ms (25.00%), Space: 77.5 MB (26.56%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} changed
+ * @return {number[]}
+ */
+const findOriginalArray = function(changed) {
+    if (changed.length % 2) return [];
+    changed.sort((a, b) => a - b);
+    
+    const freqs = new Map();
+    const res = [];
+    
+    const decrementFreqOf = n => {
+        const count = freqs.get(n);
+        count > 1 ? freqs.set(n, count - 1) : freqs.delete(n);
+    }
+    
+    changed.forEach(n => {
+        if (!freqs.has(n / 2)) {
+            freqs.set(n, (freqs.get(n) || 0) + 1);
+        } else {
+            res.push(n / 2);
+            decrementFreqOf(n / 2);
+        }
+    })
+    
+    return freqs.size ? [] : res;
+};
+
