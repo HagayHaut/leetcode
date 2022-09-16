4a7ce605ee22ba14d1f3a52334a023d61b7ff565
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-16 17:39:53 -0400

    Time: 137 ms (17.19%), Space: 45 MB (11.88%) - LeetHub

--- 733-flood-fill/733-flood-fill.js
+++ 733-flood-fill/733-flood-fill.js
@@ -6,23 +6,22 @@
  * @return {number[][]}
  */
 const floodFill = function(image, sr, sc, newColor) {
-    if (image[sr][sc] === newColor) return image;
+    const dirs = [[1,0],[-1,0],[0,1],[0,-1]],
+          n = image.length,
+          m = image[0].length,
+          oldColor = image[sr][sc];
     
-    dfs(image, sr, sc, image[sr][sc], newColor)
+    const isIB = (r, c) => r >= 0 && c >= 0 && r < n && c < m;
+    
+    const flood = (r, c) => {
+        if (isIB(r, c) && image[r][c] === oldColor) {
+            image[r][c] = newColor;
+            dirs.forEach(([R, C]) => flood(r+R, c+C));
+        }
+    }
+    
+    if (image[sr][sc] !== newColor) flood(sr, sc);
+    
     return image;
 }
 
-function dfs(image, sr, sc, color, newColor) {
-    if (
-        sr < 0 || sc < 0||
-        sr >= image.length || sc >= image[0].length ||
-        image[sr][sc] !== color
-    ) {
-        return;
-    }
-    image[sr][sc] = newColor;
-    dfs(image, sr-1, sc, color, newColor);
-    dfs(image, sr+1, sc, color, newColor);
-    dfs(image, sr, sc-1, color, newColor);
-    dfs(image, sr, sc+1, color, newColor);
-}
