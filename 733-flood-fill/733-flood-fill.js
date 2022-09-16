7e3bd1b5c64b272d24a786f415c530deeb3ce1ea
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-16 17:41:20 -0400

    Time: 122 ms (37.22%), Space: 45.4 MB (5.88%) - LeetHub

--- 733-flood-fill/733-flood-fill.js
+++ 733-flood-fill/733-flood-fill.js
@@ -7,21 +7,16 @@
  */
 const floodFill = function(image, sr, sc, newColor) {
     const dirs = [[1,0],[-1,0],[0,1],[0,-1]],
-          n = image.length,
-          m = image[0].length,
           oldColor = image[sr][sc];
     
-    const isIB = (r, c) => r >= 0 && c >= 0 && r < n && c < m;
-    
     const flood = (r, c) => {
-        if (isIB(r, c) && image[r][c] === oldColor) {
+        if (image[r] && image[r][c] === oldColor) {
             image[r][c] = newColor;
-            dirs.forEach(([R, C]) => flood(r+R, c+C));
+            dirs.forEach(([row, col]) => flood(r+row, c+col));
         }
     }
     
     if (image[sr][sc] !== newColor) flood(sr, sc);
-    
     return image;
 }
 
