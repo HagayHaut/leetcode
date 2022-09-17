77813e7d21593def9504d453cbbf3e271accc53f
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-17 12:57:04 -0400

    Time: 130 ms (6.04%), Space: 45.6 MB (8.89%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,12 +4,10 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let low = 0;
-    let high = nums.length - 1;
-    while (low <= high) {
-        const mid = Math.floor((low + high) / 2);
-        if (nums[mid] === target) return mid;
-        nums[mid] < target ? low = mid + 1 : high = mid - 1;
+    for (let [l,r] = [0,nums.length-1]; l <= r;) {
+        const m = (l + r) >> 1;
+        if (nums[m] === target) return m;
+        nums[m] < target ? l = m + 1 : r = m - 1;
     }
     return -1;
 };
