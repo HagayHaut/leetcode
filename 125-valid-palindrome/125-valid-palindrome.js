98b4684b821f33b77016e045ba90a60c332b8af7
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-17 20:45:08 -0400

    Time: 117 ms (47.38%), Space: 44.4 MB (75.37%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -4,12 +4,8 @@
  */
 const isPalindrome = function(s) {
     const str = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
-    let l = 0,
-        r = str.length - 1;
-    while (l < r) {
+    for (let l = 0, r = str.length - 1; l < r; l++, r--) {
         if (str[l] !== str[r]) return false;
-        l++;
-        r--;
     }
     return true;
 }
