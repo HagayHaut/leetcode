59e151adbf4e0755fbb294fbb7ce2d66be283b66
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-17 20:47:28 -0400

    Time: 59 ms (99.30%), Space: 44.3 MB (43.55%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
@@ -15,24 +15,18 @@
 // have to have inner loop to 
 // empty items of level in queue
 const levelOrder = function(root) {
-    const result = [],
-          q = [root];
-    
+    const result = [];
     if (!root) return result;
-    
-    while (q.length) {
+    for (let q = [root]; q.length;) {
         const qLen = q.length,
               level = [];
-        
         for (let i = 0; i < qLen; i++) {
             const node = q.shift();
             level.push(node.val);
             if (node.left) q.push(node.left);
             if (node.right) q.push(node.right);
         }
-        
         result.push(level);
     }
-    
     return result;
 };
