15f073e72e7be69adf55f77b675d64a78e5fc68f
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-19 00:01:19 -0400

    Time: 102 ms (84.70%), Space: 48.4 MB (34.23%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.js
+++ 130-surrounded-regions/130-surrounded-regions.js
@@ -5,43 +5,39 @@
 const solve = function(board) {
     const m = board.length,
           n = board[0].length,
-          safe = new Set(),
-          dirs = [[1,0],[-1,0],[0,1],[0,-1]];
+          safe = Array(m).fill().map(() => Array(n).fill(false)),
+          directions = [[1,0],[-1,0],[0,1],[0,-1]];
     
-    const isIB = (r, c) => r >= 0 && c >= 0 && r < m && c < n;
+    const isInbounds = (r, c) => r >= 0 && c >= 0 && r < m && c < n;
     
-    const dfs = (r, c) => {
-        if (
-            isIB(r, c) && 
-            !safe.has(`${r},${c}`) && 
-            board[r][c] === 'O'
-        ) {
-            safe.add(`${r},${c}`);
-            dirs.forEach(([dr,dc]) => dfs(r+dr,c+dc));
+    const markSafe = (r, c) => {
+        if (isInbounds(r, c) && board[r][c] === 'O' && !safe[r][c]) {
+            safe[r][c] = true;
+            directions.forEach(([dr, dc]) => markSafe(r + dr, c + dc));
         }
     }
     
     for (let r = 0; r < m; r++) {
-        if (board[r][0] === 'O') {
-            dfs(r, 0);
+        if (board[r][0]) {
+            markSafe(r, 0);
         }
-        if (board[r][n - 1] === 'O') {
-            dfs(r, n - 1);
+        if (board[r][n - 1]) {
+            markSafe(r, n - 1);
         }
     }
     
     for (let c = 0; c < n; c++) {
-        if (board[0][c] === 'O') {
-            dfs(0, c);
+        if (board[0][c]) {
+            markSafe(0, c);
         }
-        if (board[m - 1][c] === 'O') {
-            dfs(m - 1, c);
+        if (board[m - 1][c]) {
+            markSafe(m - 1, c);
         }
     }
     
     for (let r = 0; r < m; r++) {
         for (let c = 0; c < n; c++) {
-            if (board[r][c] === 'O' && !safe.has(`${r},${c}`)) {
+            if (board[r][c] === 'O' && !safe[r][c]) {
                 board[r][c] = 'X';
             }
         }
