916ded2ec255d47d2b4ae049e7e57e2114e7d85e
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-18 20:59:30 -0400

    Time: 95 ms (37.85%), Space: 41.6 MB (93.98%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.js
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -4,18 +4,15 @@
  */
 var generate = function(numRows) {
     let result = [[1],[1,1]]
-    if(numRows === 1) return result.slice(0,1);
-    else if ( numRows === 2) return result;
-    else {
-        for(let i = 3; i <= numRows; i++) {
-            const prevRow = result[result.length-1]
-            const currentRow = [1];
-            for(let i = 0; i < prevRow.length-1; i++) {
-                currentRow.push(prevRow[i]+prevRow[i+1])
-            }
-            currentRow.push(1)
-            result.push(currentRow)
+    if (numRows < 3) return result.slice(0,numRows);
+    for(let i = 3; i <= numRows; i++) {
+        const prevRow = result[result.length-1]
+        const currentRow = [1];
+        for(let i = 0; i < prevRow.length-1; i++) {
+            currentRow.push(prevRow[i]+prevRow[i+1])
         }
+        currentRow.push(1)
+        result.push(currentRow)
     }
     return result;
 };
