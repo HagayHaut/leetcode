e38899786acdc155a77827964b36bd137f6e6ba6
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-22 22:49:04 -0400

    Time: 72 ms (89.53%), Space: 44.2 MB (18.65%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -6,8 +6,8 @@ const longestCommonPrefix = function(strs) {
     let prefix = '';
     for (let i = 0; i < strs[0].length; i++) {
         const c = strs[0][i];
-        for (const word of strs) {
-            if (word[i] !== c) return prefix;
+        for (const str of strs) {
+            if (str[i] !== c) return prefix;
         }
         prefix += c;
     }
