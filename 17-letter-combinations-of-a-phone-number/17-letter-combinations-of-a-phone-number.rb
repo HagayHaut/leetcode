f5aee0762f1fa5c8f61dbc82fa0912d237b8f8c5
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-22 21:48:54 -0400

    Time: 131 ms (48.50%), Space: 211.2 MB (19.76%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.rb
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.rb
@@ -2,11 +2,16 @@
 # @return {String[]}
 def letter_combinations(digits)
     return [] if digits.length == 0
-    
     # map digits to their chars
-    phone = "- - abc def ghi jkl mno pqrs tuv wxyz".split
-    # group possibilities per index in subarrays
-    charsets = digits.chars.map { |d| phone[d.to_i].chars }
-    # ruby magic to get all combinations between two arrays
-    [''].product(*charsets).map(&:join)
+    phone = "- - abc def ghi jkl mno pqrs tuv wxyz".split.map { |s| s.split(//) }
+    res = []
+    backtrack '', phone, digits, res
+    res    
 end
+
+def backtrack(cur, phone, digits, res)
+    return res << cur if cur.length == digits.length
+    choices = phone[digits[cur.length].to_i]
+    choices.each { |c| backtrack cur + c, phone, digits, res }
+end
+
