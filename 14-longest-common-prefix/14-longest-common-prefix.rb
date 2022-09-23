a588edc9c6b0c7cd87b15e245fc47fd58f6ce600
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-22 22:43:52 -0400

    Time: 218 ms (5.35%), Space: 211.2 MB (11.49%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.rb
@@ -0,0 +1,12 @@
+# @param {String[]} strs
+# @return {String}
+def longest_common_prefix(strs)
+    prefix = ''
+    
+    strs[0].each_char.with_index do |char, i|
+        strs.each { |str| return prefix if str[i] != char}
+        prefix += char
+    end
+    
+    prefix
+end
