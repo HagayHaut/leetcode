e12e40aa4b73fed84f15c8c00f24616ca5c8092d
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-25 19:36:08 -0400

    Time: 90 ms (52.51%), Space: 41.8 MB (73.22%) - LeetHub

--- 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -5,9 +5,8 @@
  */
 var strStr = function(haystack, needle) {
     const len = needle.length
-    for(let i = 0; i < haystack.length; i++) {
+    for(let i = 0; i <= haystack.length - len; i++) {
         if(haystack.slice(i, i+len) === needle) return i;
-        if(i > haystack.length - len) break;
     }
     return -1;
 };
