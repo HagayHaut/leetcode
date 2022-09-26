545cc3ea7a673d2c3093c612a0b60cb3dea7842f
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-26 19:33:14 -0400

    Time: 178 ms (48.13%), Space: 212.5 MB (41.25%) - LeetHub

--- 169-majority-element/169-majority-element.rb
+++ 169-majority-element/169-majority-element.rb
@@ -1,8 +1,7 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def majority_element(nums)
-    freq = nums.tally
-    freq.each do |k, v|
+    nums.tally.each do |k, v|
         return k if v > nums.size / 2.0 
     end
 end
