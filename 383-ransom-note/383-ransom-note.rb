8050a4405f3ceb360cbfcc770a1d6abc65b902da
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-26 19:28:31 -0400

    Time: 382 ms (19.59%), Space: 214.6 MB (48.13%) - LeetHub

--- 383-ransom-note/383-ransom-note.rb
+++ 383-ransom-note/383-ransom-note.rb
@@ -2,11 +2,10 @@
 # @param {String} magazine
 # @return {Boolean}
 def can_construct(ransom_note, magazine)
-    letters = {}
-    magazine.each_char { |c| letters[c] = (letters[c] || 0) + 1 }
+    have = magazine.split(//).tally
     ransom_note.each_char do |c|
-        return false if [nil, 0].include? letters[c]
-        letters[c] -= 1
+        return false if !have.key?(c) || have[c] < 1
+        have[c] -= 1
     end
     true
 end
