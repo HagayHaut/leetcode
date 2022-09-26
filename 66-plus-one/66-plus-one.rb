1281cf1759ee6cb5449fb50e093cc4fc99e86f63
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-26 18:12:48 -0400

    Time: 87 ms (89.11%), Space: 211 MB (82.10%) - LeetHub

--- 66-plus-one/66-plus-one.rb
+++ 66-plus-one/66-plus-one.rb
@@ -1,6 +1,5 @@
 # @param {Integer[]} digits
 # @return {Integer[]}
 def plus_one(digits)
-    num = digits.map(&:to_s).join.to_i + 1
-    num.to_s.split(//).map(&:to_i)
+    (digits.join.to_i + 1).to_s.split(//).map(&:to_i)
 end
