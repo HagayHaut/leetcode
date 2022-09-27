39ac190b8a5c71bfe871782abaeb2866d6f56165
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-09-26 23:05:57 -0400

    Time: 192 ms (7.97%), Space: 211.3 MB (49.00%) - LeetHub

---
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.rb
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def max_depth(root)
+    return 0 if !root
+    1 + [max_depth(root.left), max_depth(root.right)].max
+end
