8040f9bb90b79f3b4fa126fd4ebd22aaf4092223
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-10-10 12:07:34 -0400

    Time: 347 ms (75.38%), Space: 74.5 MB (72.45%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+// greedy appraoch is faster than backtracking
+// sort by start 
+// when you find an overlap, 
+// remove meeting with later end-time
+
+const eraseOverlapIntervals = function(intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+    
+    let removed = 0;
+    let lastInterval = intervals[0];
+
+    for (let i = 1; i < intervals.length; i++) {
+        // overlap
+        if (lastInterval[1] > intervals[i][0]) {
+            removed++;
+            lastInterval = lastInterval[1] < intervals[i][1] ? lastInterval : intervals[i];
+        }
+        // no overlap
+        else {
+            lastInterval = intervals[i];
+        }
+    }
+
+    return removed;
+};
