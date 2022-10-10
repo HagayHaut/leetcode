98e80ba09db6eb675b3c401183d3cba17ad9e16b
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-10-10 12:07:02 -0400

    Time: 131 ms (48.03%), Space: 49.2 MB (62.92%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+const searchBST = function(root, val) {
+    let cur = root;
+
+    while (cur) {
+        if (cur.val === val) break;
+        cur = cur.val < val ? cur.right : cur.left;
+    }
+
+    return cur;
+};
