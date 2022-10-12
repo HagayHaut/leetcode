8eca10dd98ba9ac190974defa635ed040328c8b2
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-10-11 23:44:18 -0400

    Time: 423 ms (57.89%), Space: 76.8 MB (92.98%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.js
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.js
@@ -10,17 +10,17 @@ const findFarmland = function(land) {
     const valid = (r, c) => r >= 0 && c >= 0 && r < m && c < n;
     
     const getMaxRC = (r, c) => {
-        while (r < m - 1 && land[r+1][c] === 1) {
+        while (r < m - 1 && land[r+1][c]) {
             r++;
         }
-        while (c < n - 1 && land[r][c+1] === 1) {
+        while (c < n - 1 && land[r][c+1]) {
             c++;
         }
         return [r, c];
     }
     
     const flood = (r, c) => {
-        if (valid(r, c) && land[r][c] === 1) {
+        if (valid(r, c) && land[r][c]) {
             land[r][c] = 0;
             dirs.forEach(([dr, dc]) => flood(r + dr, c + dc));
         }
