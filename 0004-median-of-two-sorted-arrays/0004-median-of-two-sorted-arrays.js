4541b60d97eafb110688f1f207e3c56c6431b614
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-10-12 23:41:41 -0400

    Time: 98 ms (97.84%), Space: 45.9 MB (96.10%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+const findMedianSortedArrays = (nums1, nums2) => {
+    let totalLen = nums1.length + nums2.length;
+    let i = 0;
+    let j = 0;
+    let curr;
+    let last;
+
+    while (i + j <= totalLen/2) {
+        if (curr !== undefined) {
+        last = curr;
+        }
+        let x1 = nums1[i];
+        let x2 = nums2[j];
+        if (x1 === undefined) {
+            curr = x2;
+            j++;
+        } else if (x2 === undefined) {
+          curr = x1;
+          i++;
+        } else if (x1 < x2) {
+          curr = x1;
+          i++;
+        } else {
+          curr = x2;
+          j++;
+        }
+    }
+    return totalLen % 2 ? curr : (last + curr) / 2;
+};
