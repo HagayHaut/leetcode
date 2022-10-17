42ed6cf3507c91b441e3fb940aa3808bbc38d80a
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-10-17 16:14:20 -0400

    Time: 115 ms (70.78%), Space: 44.7 MB (77.85%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -0,0 +1,36 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+const isValidSudoku = function(board) {
+    const rows = {},
+          cols = {},
+          boxes = {};
+    
+    const invalid = (r, c, b, v) => {
+        return rows[r].has(v)|| 
+               cols[c].has(v) ||
+               boxes[b].has(v);
+    }
+    
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            if (board[r][c] === '.') continue;
+            
+            const b = `${~~(r/3)}${~~(c / 3)}`,
+                  val = board[r][c];
+            
+            rows[r] ||= new Set();
+            cols[c] ||= new Set();
+            boxes[b] ||= new Set();
+            
+            if (invalid(r, c, b, val)) return false;
+            
+            rows[r].add(val);
+            cols[c].add(val);
+            boxes[b].add(val);
+        }
+    }
+    
+    return true;
+};
