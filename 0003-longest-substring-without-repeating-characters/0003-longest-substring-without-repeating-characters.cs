6925ef7f49e38d11db7baff09b582dcd74b7b7aa
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-10-24 21:11:31 -0400

    Time: 110 ms (74.44%), Space: 36.9 MB (52.58%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        HashSet<char> uniqs = new HashSet<char>();
+        int res = 0;
+        int left = 0;
+        for (int right = 0; right < s.Length; right ++) {
+            while (uniqs.Contains(s[right])) {
+                uniqs.Remove(s[left]);
+                left++;
+            }
+            uniqs.Add(s[right]);
+            res = Math.Max(res, uniqs.Count);
+        }
+        return res;
+    }
+}
