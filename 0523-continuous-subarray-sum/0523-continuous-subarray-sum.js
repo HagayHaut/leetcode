6309d045cf8414e5394d06a57dfaeec7bc9ce6de
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-10-25 20:50:29 -0400

    Time: 186 ms (47.56%), Space: 65.3 MB (61.79%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.js
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.js
@@ -8,14 +8,11 @@ const checkSubarraySum = function(nums, k) {
     const map = new Map([[0, -1]]);
     for(let i = 0; i < nums.length; i++) {
         sum += nums[i];
-        if(k !== 0) sum %= k;
-        if(map.has(sum)) {
-            if(i - map.get(sum) > 1) {
-                return true;
-            } 
-        } else {
-                map.set(sum, i);
-        }
+        if (k !== 0) sum %= k;
+        if (map.has(sum)) {
+            if (i - map.get(sum) > 1) return true;
+        } 
+        else map.set(sum, i);
     }
     return false;
 };
