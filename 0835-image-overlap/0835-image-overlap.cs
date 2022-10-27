6e4e4ee2803e6f27067586f4af5b22aa99756b91
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-10-26 21:53:10 -0400

    Time: 163 ms (75.00%), Space: 39.1 MB (25.00%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public int LargestOverlap(int[][] img1, int[][] img2) {
+        int n = img1.Length;
+        
+        int helper(int xDiff, int yDiff) {
+            int num = 0;
+            
+            for (int r = 0; r < n; r++) {
+                for (int c = 0; c < n; c++) {
+                    if (
+                        0 <= c + xDiff && 
+                        c + xDiff < n && 
+                        0 <= r + yDiff &&
+                        r + yDiff < n &&
+                        img1[r + yDiff][c + xDiff] == 1 &&
+                        img2[r][c] == 1
+                    ) {
+                        num++;
+                    }
+                }
+            }
+            
+            return num;
+        }
+        
+        int res = 0;
+        
+        for (int y = -n; y < n; y++) {
+            for (int x = -n; x < n; x++) {
+                res = Math.Max(res, helper(y, x));
+            }
+        }
+        
+        return res;
+    }
+}
