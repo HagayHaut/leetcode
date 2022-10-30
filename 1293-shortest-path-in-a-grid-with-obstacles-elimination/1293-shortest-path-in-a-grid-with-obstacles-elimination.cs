ae733d3208770f18d0a275ab72ae53f7591fa02f
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-10-30 15:58:48 -0400

    Time: 123 ms (97.83%), Space: 39.6 MB (83.70%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public int ShortestPath(int[][] grid, int k) {
+        int m = grid.Length, n = grid[0].Length;
+        
+        if (k >= m + n - 2) return m + n -2;
+        
+        Queue<(int, int, int, int)> q = new Queue<(int, int, int, int)>();
+        HashSet<(int, int, int)> seen = new HashSet<(int, int, int)>();
+        q.Enqueue((0, 0, k, 0));
+        
+        while (q.Count > 0) 
+        {
+            var (i, j, K, s) = q.Dequeue();
+            
+            if ((i, j) == (m - 1, n - 1)) return s;
+            
+            (int, int)[] dirs = new (int, int)[4] { (i+1,j),(i-1,j), (i,j+1), (i,j-1)};
+            
+            foreach (var (ii, jj) in dirs) 
+            {
+                if (0 <= ii && ii < m &&0 <= jj && jj < n && K >= grid[ii][jj])
+                {
+                    if (!seen.Contains((ii, jj, K-grid[ii][jj])))
+                    {
+                        seen.Add((ii, jj, K-grid[ii][jj]));
+                        q.Enqueue((ii, jj, K-grid[ii][jj], s + 1));
+                    }
+                }
+            }
+            
+        }
+        
+        return -1;
+    }
+}
