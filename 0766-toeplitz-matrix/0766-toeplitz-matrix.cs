6f708f8bae5271be5a0658156cf04ff9549149d1
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-10-30 23:23:55 -0400

    Time: 189 ms (39.25%), Space: 41.4 MB (45.79%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cs
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cs
@@ -6,29 +6,31 @@ public class Solution {
             return r >= 0 && c >= 0 && r < m && c < n;
         }
         
-        for (int r = 0; r < m; r++)
-        {
-            int rr = r, cc = 0, val = matrix[r][0];
-            
-            while (isInbounds(rr + 1, cc + 1))
+        bool checkDiagonal(int r, int c) {
+            int val = matrix[r][c];
+            while (isInbounds(r + 1, c + 1))
             {
-                if (matrix[++rr][++cc] != val)
+                if (matrix[++r][++c] != val)
                 {
                     return false;
                 }
+            }
+            return true;
+        }
+        
+        for (int r = 0; r < m; r++)
+        {
+            if (!checkDiagonal(r, 0))
+            {
+                return false;
             }
         }
         
         for (int c = 0; c < n; c++)
         {
-            int rr = 0, cc = c, val = matrix[0][c];
-            
-            while (isInbounds(rr + 1, cc + 1))
+            if (!checkDiagonal(0, c))
             {
-                if (matrix[++rr][++cc] != val) 
-                {
-                    return false;
-                }
+                return false;
             }
         }
         
