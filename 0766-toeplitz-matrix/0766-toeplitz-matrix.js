a6d32297463a65cc3c06b46b3a87e6d98bf223a0
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-10-31 14:19:29 -0400

    Time: 155 ms (10.90%), Space: 44 MB (71.80%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.js
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.js
@@ -4,24 +4,23 @@
  */
 const isToeplitzMatrix = function(matrix) {
     const m = matrix.length, n = matrix[0].length;
+    
     const isIB = (r, c) => r >= 0 && c >= 0 && r < m && c < n;
     
-    for (let r = 0; r < m; r++) {
-        let cc = 0, rr = r, val = matrix[r][0];
-        while (isIB(rr + 1, cc + 1)) {
-            rr++; 
-            cc++;
-            if (matrix[rr][cc] !== val) return false;
+    const checkDiagonal = (r, c) => {
+        const val = matrix[r][c];
+        while (isIB(r + 1, c + 1)) {
+            if (matrix[++r][++c] !== val) return false;
         }
+        return true;
+    }
+    
+    for (let r = 0; r < m; r++) {
+        if (!checkDiagonal(r, 0)) return false;
     }
     
     for (let c = 1; c < n; c++) {
-        let rr = 0, cc = c, val = matrix[0][c];
-        while (isIB(rr + 1, cc + 1)) {
-            rr++;
-            cc++;
-            if (matrix[rr][cc] !== val) return false;
-        }
+        if (!checkDiagonal(0, c)) return false;
     }
     
     return true;
