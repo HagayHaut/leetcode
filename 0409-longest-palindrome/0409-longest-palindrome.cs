45b747fe1fb14a9556a1fbc18fb4e925602e488d
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-11-03 19:15:51 -0400

    Time: 104 ms (61.01%), Space: 34.6 MB (93.28%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public int LongestPalindrome(string s) 
+    {
+        IDictionary<char, int> counts = new Dictionary<char, int>();
+        
+        foreach (char c in s)
+        {
+            if (!counts.ContainsKey(c))
+            {
+                counts.Add(c, 0);
+            }
+            counts[c]++;
+        }
+        
+        bool flag = false;
+        int res = 0;
+        
+        foreach (var entry in counts)
+        {
+            if (entry.Value % 2 == 0)
+            {
+                res += entry.Value;
+            }
+            else {
+                flag = true;
+                res += (entry.Value - 1);
+            }
+        }
+        
+        return flag ? res + 1 : res;
+    }
+}
