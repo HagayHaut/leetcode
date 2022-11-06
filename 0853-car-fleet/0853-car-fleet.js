cc2ad0d6e042402c210e04e91976df66a5861743
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-11-06 10:41:50 -0500

    Time: 554 ms (34.40%), Space: 70 MB (88.87%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} target
+ * @param {number[]} position
+ * @param {number[]} speed
+ * @return {number}
+ */
+const carFleet = function(target, position, speed) {
+    // map position to speed
+    const map = {};
+    let res = 0;
+    position.forEach((p, i) => map[p] = speed[i]);
+    position.sort((a, b) => a - b);
+    
+    const hasCollision = (n) => {
+        return (map[position.at(-1)] - map[n]) * (target - n) / map[n] >= n - position.at(-1);
+    }
+    
+    while (position[0] !== undefined) {
+        const p = position.pop();
+        res++;
+        while (hasCollision(p)) position.pop();
+    }
+    
+    return res;
+};
