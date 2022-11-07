075688a7dd7b5a771f04184035ee10498066a688
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-11-06 20:52:24 -0500

    Time: 79 ms (79.46%), Space: 41.7 MB (79.23%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.js
+++ 1323-maximum-69-number/1323-maximum-69-number.js
@@ -3,11 +3,5 @@
  * @return {number}
  */
 const maximum69Number  = function(num) {
-    const digits = [...`${num}`];
-    const sixIdx = digits.indexOf('6');
-    
-    if (sixIdx === -1) return num;
-    
-    digits[sixIdx] = '9';
-    return Number(digits.join(''));
+    return Number(`${num}`.replace(/6/, '9'));
 };
