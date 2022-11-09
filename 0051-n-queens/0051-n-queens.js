ddad4ebf69d12fbf776cb18d259708d135602674
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-11-08 22:09:11 -0500

    Time: 161 ms (23.86%), Space: 49.1 MB (17.18%) - LeetHub

--- 0051-n-queens/0051-n-queens.js
+++ 0051-n-queens/0051-n-queens.js
@@ -8,17 +8,14 @@ const solveNQueens = function(n) {
                                       
     const backtrack = (r, cols = new Set(), posDiag = new Set(), negDiag = new Set()) => {
         
-        const shouldSkip = (r, c) => {
-            return cols.has(c) || posDiag.has(r + c) || negDiag.has(r - c);
-        }
-        
         if (r === n) {
             return res.push(board.map(row => row.join('')));
         }
         
         for (let c = 0; c < n; c++) {
-            if (shouldSkip(r, c)) continue;
-            
+            if (cols.has(c) || posDiag.has(r + c) || negDiag.has(r - c)) {
+                continue;
+            }
             board[r][c] = 'Q';
             backtrack(r + 1, new Set([...cols, c]), new Set([...posDiag, r + c]), new Set([...negDiag, r - c]));
             board[r][c] = '.';
