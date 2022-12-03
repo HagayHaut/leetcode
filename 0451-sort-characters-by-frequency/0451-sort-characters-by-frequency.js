24163e50ffb992524344e6c82c7d0ae3d084a22c
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2022-12-03 18:42:54 -0500

    Time: 111 ms (80.48%), Space: 45.4 MB (70.47%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.js
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.js
@@ -3,18 +3,12 @@
  * @return {string}
  */
 const frequencySort = function(s) {
-    
-    const freqToStr = ([c, f]) => c.repeat(f);
-    
-    const freqEntries = Object.entries(
+     return Object.entries(
         [...s].reduce((obj, c) => {
             obj[c] = obj[c] ? obj[c] + 1 : 1;
             return obj;
-        }, {})
-    );
-    
-    return freqEntries
+        }, {}))
             .sort((a, z) => z[1] - a[1])
-            .map(freqToStr)
+            .map(([c, f]) => c.repeat(f))
             .join('');
 };
