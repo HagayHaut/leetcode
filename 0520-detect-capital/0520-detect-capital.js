360c3b5568d7f2450deb30a09d8f4484fbc2d23f
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2023-01-01 22:33:45 -0500

    Time: 66 ms (89.89%), Space: 43.8 MB (5.05%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.js
+++ 0520-detect-capital/0520-detect-capital.js
@@ -6,10 +6,8 @@ const isLower = c => c === c.toLowerCase();
 const isUpper = c => c === c.toUpperCase();
 
 var detectCapitalUse = function(word) {
-    if (isLower(word[0])) {
-        return [...word].every(isLower)
-    } else {
-        const [_, ...rest] = [...word];
-        return rest.every(isLower) || rest.every(isUpper);
-    }
+    const [first, ...rest] = [...word];
+    return isLower(first)
+        ? [...rest].every(isLower)
+        : (rest.every(isLower) || rest.every(isUpper));
 };
