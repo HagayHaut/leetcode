9039887a87a1dcc3961ac65f6b02dfae09308b5b
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2023-01-07 23:22:44 -0500

    Time: 145 ms (92.61%), Space: 45.8 MB (23.61%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cs
+++ 0733-flood-fill/0733-flood-fill.cs
@@ -7,6 +7,13 @@ public class Solution
         int m = image.Length;
         int n = image[0].Length;
         int oldColor = image[sr][sc];
+        List<List<int>> directions = new()
+        {
+            new() {1, 0},
+            new() {-1, 0},
+            new() {0, 1},
+            new() {0, -1},
+        };
         Func<int,int,bool> isValid = (r, c) => r >= 0 && c >= 0 && r < m && c < n;
         
         void flood(int r, int c)
@@ -15,10 +22,7 @@ public class Solution
             {
                 image[r][c] = newColor;
                 
-                flood(r, c + 1);
-                flood(r, c - 1);
-                flood(r + 1, c);
-                flood(r - 1, c);
+                directions.ForEach(d => flood(r + d[0], c + d[1]));
             }
         }
         
