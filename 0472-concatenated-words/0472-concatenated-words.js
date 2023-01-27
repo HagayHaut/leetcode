9de64e8ed51170e7dd3267eb1ab99cfe2c64a60f
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2023-01-27 18:11:20 -0500

    Time: 205 ms (66.67%), Space: 54.8 MB (59.26%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.js
+++ 0472-concatenated-words/0472-concatenated-words.js
@@ -6,19 +6,16 @@ const findAllConcatenatedWordsInADict = function(words) {
     const dictionary = new Set(words);
     const res = [];
     
-    words.forEach(word => {
+    words.forEach((word, i) => {
         const n = word.length;
-        const dp = Array(n + 1).fill(false);
+        const dp = Array(word.length + 1).fill(false);
         dp[0] = true;
-        
         for (let i = 1; i <= n; i++) {
-            for (let j = (i === n ? 1 : 0); j < i; j++) {
-                if (!dp[i]) {
-                    dp[i] = dp[j] && dictionary.has(word.slice(j, i));
-                }
+            for (let j = (i === n ? 1 : 0); !dp[i] && j < i; j++) {
+                dp[i] = dp[j] && dictionary.has(word.slice(j, i));
             }
-            if (dp[n]) res.push(word);
         }
+        if (dp[n]) res.push(word);
     })
     
     return res;
