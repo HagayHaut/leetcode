2828b2b424e182ade4c7cc7d90671d7b55d0e894
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2023-02-28 19:51:05 -0500

    Time: 266 ms (75.60%), Space: 66.3 MB (26.39%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const sortArray = function(nums) {
+    return mergeSort(nums);
+};
+
+function merge(a1, a2) {
+    let merged = [],
+        i = 0,
+        j = 0;
+    
+    while (i < a1.length && j < a2.length) {
+        if (a1[i] < a2[j]) merged.push(a1[i++]);
+        else merged.push(a2[j++]);
+    }
+    
+    while (i < a1.length) merged.push(a1[i++]);
+    while (j < a2.length) merged.push(a2[j++]);
+    
+    return merged;
+}
+
+function mergeSort(arr) {
+    if (arr.length < 2) return arr;
+    const mid = Math.floor(arr.length / 2),
+          left = mergeSort(arr.slice(0,mid)),
+          right = mergeSort(arr.slice(mid));
+    return merge(left,right);
+}
