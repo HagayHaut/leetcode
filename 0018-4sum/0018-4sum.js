f996a079832e6f25cf96115d3d16c7948fdaf84b
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2023-03-06 20:38:31 -0500

    Time: 104 ms (48.60%), Space: 44.8 MB (34.73%) - LeetHub

--- 0018-4sum/0018-4sum.js
+++ 0018-4sum/0018-4sum.js
@@ -16,18 +16,17 @@ const fourSum = function(nums, target) {
     const kSum = (k, start, trgt) => {
         if (k !== 2) {
             // leave 3 values at end to form quad
-            for (let i = start; i<nums.length-k+1; i++) {
+            for (let i = start; i< nums.length - k + 1; i++) {
                 // avoid dups
-                if (i > start && nums[i] === nums[i - 1]) {
-                    continue;
-                }
+                if (i > start && nums[i] === nums[i - 1]) continue;
+                
                 // backtracking
                 quad.push(nums[i]);
                 kSum(k - 1, i + 1, trgt - nums[i]);
                 quad.pop();
             }
         }
-        // base case => Two Sum II
+        // base case -> Two Sum II
         else {
             let l = start,
             r = nums.length - 1;
