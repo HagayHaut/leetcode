cbd2bd4a0dccb5718d0ccd40da088ed6048c5fb7
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2023-03-06 20:31:31 -0500

    Time: 93 ms (82.24%), Space: 44.5 MB (88.96%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.js
+++ 0076-minimum-window-substring/0076-minimum-window-substring.js
@@ -4,23 +4,20 @@
  * @return {string}
  */
 const minWindow = function(s, t) {
-  const map = {};
+  const map  = {};
   const sLen = s.length;
   const tLen = t.length;
-  let count = tLen;
-  let min = Infinity;
-  let head = 0;
-  let left = 0;
-  let right = 0;
+  let count  = tLen; 
+  let min    = Infinity;
+  let head   = 0;
+  let left   = 0;
+  let right  = 0;
 
   if (!sLen || !tLen) return '';
 
   for (let i = 0; i < tLen; i++) {
-    if (map[t[i]] === undefined) {
-      map[t[i]] = 1
-    } else {
-      map[t[i]]++;
-    }
+    if (map[t[i]] === undefined) map[t[i]] = 1;
+    else map[t[i]]++;
   }
 
   while (right < sLen) {
