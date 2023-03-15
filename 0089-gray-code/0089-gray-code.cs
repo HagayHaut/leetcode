3244aa8a070192c209074294e6a1fe186de4bb30
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2023-03-14 20:19:52 -0400

    Time: 83 ms (100.00%), Space: 47 MB (40.91%) - LeetHub

--- 0089-gray-code/0089-gray-code.cs
+++ 0089-gray-code/0089-gray-code.cs
@@ -1,7 +1,10 @@
-public class Solution {
-    public IList<int> GrayCode(int n) {
+public class Solution 
+{
+    public IList<int> GrayCode(int n) 
+    {
         var res = new List<int> { 0 };
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             int size = res.Count;
             for (int k = size - 1; k >= 0; k--) 
                 res.Add(res[k] | 1 << i);
