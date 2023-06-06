1f77fa9be877b4b450092df143e349d22ae01fd3
Hagay Haut <hagayhaut@gmail.com>
2023-06-05 20:43:49 -0400

    unnecessary abstraction

--- reorganize_repo.rb
+++ reorganize_repo.rb
@@ -1,9 +1,5 @@
 require 'fileutils'
 
-def move_to_folder(dir_to_move, name_of_dir, difficulty)
-    FileUtils.mv(dir_to_move, "#{__dir__}/#{difficulty}/#{name_of_dir}")
-end
-
 Dir.each_child(__dir__) do |child_dir_name|
     if child_dir_name.match /^\d+/ 
         cur_dir = "#{__dir__}/#{child_dir_name}"
@@ -15,7 +11,7 @@ Dir.each_child(__dir__) do |child_dir_name|
                     when 'M' 'medium'
                     else 'hard'
                     end
-                move_to_folder(cur_dir, child_dir_name, difficulty)
+                FileUtils.mv(cur_dir, "#{__dir__}/#{difficulty}/#{child_dir_name}")
             end
         end
     end
