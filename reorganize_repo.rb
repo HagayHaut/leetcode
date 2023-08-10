1149cbd4bc6941c48ee19b325a57cc2ee08263ae
Hagay Haut <hagayhaut@gmail.com>
2023-08-09 22:26:39 -0400

    reorg

--- reorganize_repo.rb
+++ reorganize_repo.rb
@@ -1,9 +1,12 @@
 require 'fileutils'
 
 # moves new solutions into difficulty folders
+# and deletes auto generated NOTES.md files
 Dir.each_child(__dir__) do |child_dir_name|
     if child_dir_name.match /^\d+/ 
         cur_dir = "#{__dir__}/#{child_dir_name}"
+        difficulty = '' # to be a flag in outer scope
+        has_notes = false
         Dir.each_child(cur_dir) do |filename|
             if filename == 'README.md'
                 first_line = File.open("#{cur_dir}/#{filename}", &:readline)
@@ -12,8 +15,11 @@ Dir.each_child(__dir__) do |child_dir_name|
                     when 'M' 'medium'
                     else 'hard'
                 end
-                FileUtils.mv(cur_dir, "#{__dir__}/#{difficulty}/#{child_dir_name}")
+            elsif filename == 'NOTES.md'
+                has_notes = true
             end
         end
+        FileUtils.rm_f(["#{cur_dir}/NOTES.md"]) if has_notes
+        FileUtils.mv(cur_dir, "#{__dir__}/#{difficulty}/#{child_dir_name}") if difficulty != ''
     end
 end
