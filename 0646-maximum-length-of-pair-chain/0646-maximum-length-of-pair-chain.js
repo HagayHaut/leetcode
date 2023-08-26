b52c01398c444100abf044d0d893c4d44d9dbf0c
Hagay Haut <100384848+HagayHaut@users.noreply.github.com>
2023-08-25 20:36:04 -0400

    Time: 117 ms (35.85%), Space: 46.4 MB (40.57%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.js
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.js
@@ -4,21 +4,16 @@
  */
 var findLongestChain = function(pairs) {
     const n = pairs.length;
-    const helper = (i, memo) => {
-        if (memo[i]) return memo[i];
-        memo[i] = 1;
+    pairs.sort((a, z) => a[0] - z[0]);
+    let res = 1;
+    const dp = Array(n).fill(1);
+    for (let i = n - 1; i > -1; i--) {
         for (let j = i + 1; j < n; j++) {
             if (pairs[i][1] < pairs[j][0]) {
-                memo[i] = Math.max(memo[i], helper(j, memo) + 1);
+                dp[i] = Math.max(dp[i], 1 + dp[j]);
             }
         }
-        return memo[i];
-    }
-    pairs.sort((a, z) => a[0] - z[0]);
-    let res = 0;
-    const memo = Array(n).fill(0);
-    for (let i = 0; i < n; i++) {
-        res = Math.max(res, helper(i, memo));
+        res = Math.max(res, dp[i]);
     }
     return res;
 };
